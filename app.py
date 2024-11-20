9eac13e29825f1acf91f12c8abb4e5789e7bab35
hwanheo <38911266+hwanheo97@users.noreply.github.com>
2024-11-20 17:11:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -56,92 +56,92 @@ bensmenus =[
     {
     "name": "김치 볶음밥",
     "type": "한식",
-    "image_url":".\images\kimchi_fried.jpg",
+    "image_url":"./images/kimchi_fried.jpg",
     "recipe":"재료:마늘,파,신김치,찬밥,고추장,계란"
 },
 {
     "name": "파스타",
     "type": "양식",
-    "image_url":".\images\menu_12.jpg",
+    "image_url":"./images/menu_12.jpg",
     "recipe":"재료:마늘,양파,해산물,토마토소스,파스타면,식용유"
 },
 {
     "name": "열무국수",
     "type": "한식",
-    "image_url":".\images\menu_14.jpg",
+    "image_url":"./images/menu_14.jpg",
     "recipe":"재료:열무육수,열무,면사리,오이,깨"
 },
 {
     "name": "제육볶음",
     "type": "한식",
-    "image_url":".\images\menu_10.jpg",
+    "image_url":"./images/menu_10.jpg",
     "recipe":"마늘,파,양파,돼지고기,고추장"
 },
 {
     "name": "파스타",
     "type": "양식",
-    "image_url":".\images\menu_13.jpg",
+    "image_url":"./images/menu_13.jpg",
     "recipe":"재료:마늘,양파,새우,오징어,토마토소스,파스타면,식용유"
 },
 {
     "name": "스파게티",
     "type": "양식",
-    "image_url":".\images\menu_15.jpg",
+    "image_url":"./images/menu_15.jpg",
     "recipe":"재료:마늘,양파,피마으토마토소스,스파게티면 ,식용유"
 },
 {
     "name": "칼국수",
     "type": "한식",
-    "image_url":".\images\Knif-cut_noodle.jpg",
+    "image_url":"./images/Knif-cut_noodle.jpg",
     "recipe":"재료:마늘,양파,청양고추,육수,칼국수면"
 },
 {
     "name": "우동",
     "type": "중식",
-    "image_url":".\images\menu_03.jpg",
+    "image_url":"./images/menu_03.jpg",
     "recipe":"재료:마늘,양파,새우,오징어,육수,면,게란"
 },
 {
     "name": "계란말이",
     "type": "일식",
-    "image_url":".\images\menu_04.jpg",
+    "image_url":"./images/menu_04.jpg",
     "recipe":"재료: 파,당근,계란"
 },
 {
     "name": "짜장면",
     "type": "중식",
-    "image_url":".\images\menu_05.jpg",
+    "image_url":"./images/menu_05.jpg",
     "recipe":"춘장,감자,양파,돼지고기,오이,꺠"
 },
 {
     "name": "파스타",
     "type": "양식",
-    "image_url":".\images\menu_06.jpg",
+    "image_url":"./images/menu_06.jpg",
     "recipe":"재료:마늘,양파,새우,오징어,토마토소스,파스타,식용유"
 },
 {
     "name": "냉콩국수",
     "type": "한식",
-    "image_url":".\images\menu_07.jpg",
+    "image_url":"./images/menu_07.jpg",
     "recipe":"육수,김치,김,면,깨"
 },
 {
     "name": "수박국수",
     "type": "한식",
-    "image_url":".\images\menu_08.jpg",
+    "image_url":"./images/menu_08.jpg",
     "recipe":"수박,블루베리,오인,면사리,깨"
 },
 {
     "name": "파스타",
     "type": "양식",
-    "image_url":".\images\menu_11.jpg",
+    "image_url":"./images/menu_11.jpg",
     "recipe":"재료:마늘,양파,새우,오징어,토마토소스,파스타,식용유"
 },
 
 {
     "name": "김치 볶음밥",
     "type": "한식",
-    "image_url":".\images\Kimchi_Fried_Rice .jpg",
+    "image_url":"./images/Kimchi_Fried_Rice .jpg",
     "recipe":"재료:마늘,파,신김치,찬밥,고추장,계란"
 },
 ]
